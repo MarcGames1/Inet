60fe2f0bfbefabc017ddb254bccdf56dbb2693d5
MarcGames1 <George.marcu20@gmail.com>
2022-09-28 12:03:41 +0300

    responsive

--- src/components/list-pictures-section/index.tsx
+++ src/components/list-pictures-section/index.tsx
@@ -7,34 +7,21 @@ function getIcon (FeatureSvg:any, index:number) {
 }
 
 
-const ListWithPic = 
-({indemn, titlu, listItems, }: 
-  {
-     
-    indemn:string;
-    titlu:string;
-    listItems:any;
-   
-  }) => (
-  <section id='list' className={tw(`lg:py-28 pt-28 overflow-hidden`)}>
+const ListWithPic = ({ indemn, titlu, listItems }: { indemn: string; titlu: string; listItems: any }) => (
+  <section id="list" className={tw(`lg:py-28 pt-28 overflow-hidden`)}>
     <div className={tw(`max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 bg-white`)}>
       <div className={tw(`mb-16 text-center`)}>
         <span className={tw(`text-base text-indigo-600 font-semibold tracking-wide uppercase`)}>{indemn}</span>
-        <h2 className={tw(`mt-2 pb-4 text-5xl lg:text-7xl font-bold tracking-tight text-gray-900`)}>
-          {titlu}
-        </h2>
+        <h2 className={tw(`mt-2 pb-4 text-5xl lg:text-7xl font-bold tracking-tight text-gray-900`)}>{titlu}</h2>
       </div>
       <div className={tw(`flex  flex-wrap -mx-8 items-center`)}>
-        
-          <ul>
-            {listItems.map((item: any, index: number) => (
-             
-            
-                <div key={item.title} className={tw(`flex ${ (index % 2) ? 'flex-row-reverse' : 'flex-row' }`)}>
+        <ul>
+          {listItems.map((item: any, index: number) => (
+            <div key={item.title} className={tw(`flex flex-col ${index % 2 ? 'lg:flex-row-reverse' : 'lg:flex-row'}`)}>
               <li className={tw(`flex items-center w-full lg:w-1/2 px-8`)} key={item.title}>
                 <div className={tw(`flex px-4 content-around `)}>
                   <span
-                    className={tw(`flex w-16 h-16 mx-auto items-center
+                    className={tw(`hidden md:flex w-16 h-16 mx-auto items-center
                       justify-center text-2xl font-bold rounded-full
                       bg-blue-50 text-blue-500`)}
                   >
@@ -46,16 +33,14 @@ const ListWithPic =
                   <p className={tw(`text-gray-500 leading-loose`)}>{item.description}</p>
                 </div>
               </li>
-       <div className={tw(`items-center	w-full lg:w-1/2 px-8`)}>
-          <div className={tw(`lg:mb-12 lg:mb-0 pb-12 lg:pb-0 mt-16 lg:mt-0 mx-6 lg:mx-0`)}>
-            
-            <item.Icon width="100%" height="100%" />
-          </div>
-        </div> 
-        </div>
-            ))}
-          </ul>
-        
+              <div className={tw(`items-center	w-full lg:w-1/2 px-8`)}>
+                <div className={tw(`lg:mb-12 lg:mb-0 pb-12 lg:pb-0 mt-16 lg:mt-0 mx-6 lg:mx-0`)}>
+                  <item.Icon width="100%" height="100%" />
+                </div>
+              </div>
+            </div>
+          ))}
+        </ul>
       </div>
     </div>
   </section>
