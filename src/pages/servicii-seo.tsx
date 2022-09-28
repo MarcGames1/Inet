23daa61822c9928641e2af733cc93ca92c5d3176
MarcGames1 <George.marcu20@gmail.com>
2022-09-28 15:14:14 +0300

    x

--- src/pages/servicii-seo.tsx
+++ src/pages/servicii-seo.tsx
@@ -23,18 +23,22 @@ import { serviciiSeo } from '@/data/homepage';
 
 const ServiciiSeo = () => {
   return (
-    <Page>
-      <NextSeo
-        title="Servicii SEO •
+    <Suspense fallback={`Loading...`}>
+      <Page>
+        <NextSeo
+          title="Servicii SEO •
           Marketing Online cu rezultate ⭐ IwebAgency.ro"
-        description="Servicii Seo care genereaza rezultate exceptionale •  aducem trafic organic si crestem profitul ✔️ Solicita acum o oferta  ⭐ IwebAgency.ro."
-        canonical="https://iwebagency.ro/"
-      />
+          description="Servicii Seo care genereaza rezultate exceptionale •  aducem trafic organic si crestem profitul ✔️ Solicita acum o oferta  ⭐ IwebAgency.ro."
+          canonical="https://iwebagency.ro/"
+        />
 
-      <Header h1={serviciiSeo.s1.h1} subtitlu={serviciiSeo.s1.subtitlu} variant={serviciiSeo.s1.cta} />
-      <Calendly />
-      <FeatureSection h2={serviciiSeo.s2.h2} indemn={serviciiSeo.s2.indemn} list={serviciiSeo.s2.listaServicii} />
-    </Page>
+        <Header h1={serviciiSeo.s1.h1} subtitlu={serviciiSeo.s1.subtitlu} variant={serviciiSeo.s1.cta} />
+        <main>
+          <Calendly />
+          <FeatureSection h2={serviciiSeo.s2.h2} indemn={serviciiSeo.s2.indemn} list={serviciiSeo.s2.listaServicii} />
+        </main>
+      </Page>
+    </Suspense>
   );
 };
 
