a1d9acdfb702cd1eebc0c10395ecf86219e8ab1e
MarcGames1 <George.marcu20@gmail.com>
2022-09-28 13:26:59 +0300

    x

--- src/components/calendly/index.jsx
+++ src/components/calendly/index.jsx
@@ -3,40 +3,36 @@ import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import { tw } from 'twind'
 import Script from 'next/script'
-const InlineWidget  = dynamic(() => import('react-calendly').then((c)=>c.InlineWidget), {
+const PopupWidget  = dynamic(() => import('react-calendly').then((c)=>c.InlineWidget), {
   loading: () => <div>Loading...</div>,
 })
 
 const Calendly = (h2) => {
   
   
-const [ishidden, setIsHidden] = useState(true)
+const [ishidden, setIsHidden] = useState(false)
 
-useEffect(() =>{
-
-  if (typeof window !== 'undefined'){
-    window.onload = (e) => {
-      setIsHidden(false)
-    };
-  }
-}, [typeof window])
 
 
 
  return (
   <>
 
-     {ishidden ? <InlineWidget
+      <PopupWidget
        url="https://calendly.com/alexandru-marcu/consultanta-gratuita-seo?primary_color=4f46e5"
-       pageSettings={{
-         backgroundColor: 'ffffff',
-         hideEventTypeDetails: false,
-         hideLandingPageDetails: false,
-         primaryColor: '4f46e5',
-         textColor: '1f2927',
-         hideGdprBanner: true,
-       }}
-     /> : null}
+       rootElement= {null}
+       text="Click here to schedule!"
+       textColor="#ffffff"
+       color="#4f46e5"
+      //  pageSettings={{
+      //    backgroundColor: 'ffffff',
+      //    hideEventTypeDetails: false,
+      //    hideLandingPageDetails: false,
+      //    primaryColor: '4f46e5',
+      //    textColor: '1f2927',
+      //    hideGdprBanner: true,
+      //  }}
+     /> 
      
   </>
     
