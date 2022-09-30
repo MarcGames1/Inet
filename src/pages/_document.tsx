24003dd02c90059155964e9a3f83f82fcc204bea
MarcGames1 <George.marcu20@gmail.com>
2022-09-30 21:14:02 +0300

    gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/do
 import { setup } from 'twind';
 import { asyncVirtualSheet, getStyleTagProperties } from 'twind/server';
 import twindConfig from '../twind.config';
-
+import Script from 'next/script';
 const sheet = asyncVirtualSheet();
 
 setup({ ...twindConfig, sheet });
@@ -36,14 +36,26 @@ class MyDocument extends Document {
     return (
       <Html lang="ro">
         <Head>
-         
-          
-
+          <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KT4SFNV');`,
+            }}
+          ></Script>
         </Head>
         <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KT4SFNV"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}
+          ></noscript>
           <Main />
           <NextScript />
-
         </body>
       </Html>
     );
