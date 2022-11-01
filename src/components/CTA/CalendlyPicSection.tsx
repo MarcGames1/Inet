026e9f0e470235027bec5a7c1f7b8a1c259e39be
MarcGames1 <George.marcu20@gmail.com>
2022-11-02 00:32:26 +0200

    x

--- src/components/CTA/CalendlyPicSection.tsx
+++ src/components/CTA/CalendlyPicSection.tsx
@@ -7,9 +7,9 @@ const meImg = '/images/alexandru-marcu-specialist-seo.png'
 
 const CalendlyPicSection = ({titlu, text, btntxt}:{titlu:string, text:string, btntxt?:string | undefined}) => {
   return (
-    <section id="cta" className={tw(`lg:py-28 w-full min-h-screen  relative pt-28  h-screen overflow-hidden`)}>
-      <div className={tw(`h-full w-full mx-auto p-4 sm:p-6 lg:p-8`)}>
-        <div className={tw('lg:grid lg:h-auto lg:grid-cols-3 w bg-gradient-to-r from-indigo-100 to-indigo-300 ')}>
+    <section id="cta" className={tw(`lg:py-28 h-screen min-h-screen  relative pt-28  h-screen overflow-hidden`)}>
+      <div className={tw(`h-full grid row w-full mx-auto p-4 sm:p-6 lg:p-8`)}>
+        <div className={tw('h-full  grid grid-cols-3 w bg-gradient-to-r from-indigo-100 to-indigo-300 ')}>
           <div
             className={tw(
               'lg:h-auto justify-around items-center  text-xl col-span-3 lg:col-span-2 flex flex-col text-gray-900',
@@ -21,7 +21,7 @@ const CalendlyPicSection = ({titlu, text, btntxt}:{titlu:string, text:string, bt
             <p className={tw('mx-16 flex flex-row')}>{text}</p>
             <CalendlyButton text={btntxt} primary={undefined} modifier={undefined} />
           </div>
-          <div className={tw('h-screen md:hidden block relative  col-span-1 justify-self-center self-stretch ')}>
+          <div className={tw('relative flex h-96  ')}>
             <Image
               src={meImg}
               alt="Alexandru Marcu Specialist Seo"
