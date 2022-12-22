104c953c8a97ebc2c06cf9c31588364c62d37508
MarcGames1 <George.marcu20@gmail.com>
2022-12-22 14:20:20 +0200

    Update [...slug].tsx

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -12,10 +12,10 @@ export default function Home() {
       </Head>
       <Page>
         <div className="flex container items-center">
-          <span className={tw('text-red-500 font-bold text-9xl')}>404</span>
+          <span className={tw('hidden lg:flex text-red-500 font-bold text-9xl')}>404</span>
 
           <div className={tw('p-5 h-2/6 flex flex-col m-2 m-auto h-2/6')}>
-          <h1 className={tw('text-center m-3 font-semibol text-3xl')}>Aceasta Pagina va fi disponibila incurand</h1>
+            <h1 className={tw('text-center m-3 font-semibol text-3xl')}>Aceasta Pagina va fi disponibila incurand</h1>
             <Image objectFit="contain" layout="intrinsic" width={700} height={400} src="/loading.gif" />
           </div>
         </div>
