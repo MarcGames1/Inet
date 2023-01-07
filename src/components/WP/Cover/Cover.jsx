7fbe639437bf3a23034ff625da3dfaff15338225
MarcGames1 <George.marcu20@gmail.com>
2023-01-08 00:00:56 +0200

    x

--- src/components/WP/Cover/Cover.jsx
+++ src/components/WP/Cover/Cover.jsx
@@ -5,18 +5,18 @@ export const Cover = ({ children, background }) => {
  
     console.log(useBlogPostContext);
     const { featuredImage } = useBlogPostContext();
-
+    
   return (
     <div
       className={tw('z-0 max-h-screen text-white bg-slate-800 relative min-h-[400px] flex justify-center items-center')}
     >
       {(!!background || !!featuredImage) && (
         <Image
-          alt="Cover"
-          src={background || featuredImage}
+          alt={featuredImage.node.altText || 'Cover'}
+          src={background || featuredImage.node.sourceUrl}
           layout="fill"
           objectFit="cover"
-          className={tw("mix-blend-soft-light z-0")}
+          className={tw('mix-blend-soft-light z-0')}
         />
       )}
       <div className={tw('max-w-5xl')}>{children}</div>
--- src/context/blogPost.jsx
+++ src/context/blogPost.jsx
@@ -2,30 +2,59 @@ import { createContext, useContext } from 'react';
 import dynamic from 'next/dynamic';
 import { tw } from 'twind';
 
-
+import Image from 'next/image';
 import Head from 'next/head';
 import Navigation from '@/components/navigation';
 
 import Footer from '@/components/footer';
-
+import Link from 'next/link';
 
 const Page = dynamic(() => import('@/components/page'));
 const BlogPostContext = createContext();
 
 export const PostWrapper = ({ value, children }) => {
+  console.log('Vluw', value.author?.node?.dateSuplimentareAutor);
+
+
+
+ const urlAutor = '/echipa/alexandru-marcu' 
+ const pozaAutor = {
+   altText: value.author?.node?.dateSuplimentareAutor?.authorpic?.altText,
+   url: value.author?.node?.dateSuplimentareAutor?.authorpic?.mediaItemUrl,
+ };
+  const { descriereAutor, functie, instagram, tikTok, youtube } = value.author?.node?.dateSuplimentareAutor;
+  const author = value.author || undefined;
   return (
     <BlogPostContext.Provider value={value}>
       <Navigation />
-        <>
+      <>
         <div className={tw('grid grid-cols-12 gap3	')}>
           <aside className={tw('col-span-2')}>test </aside>
           <main className={tw('col-span-8')}>{children}</main>
 
-          <aside className={tw('col-span-2')}>test </aside>
+          <aside className={tw('container col-span-2')}>
+            <div className={tw('m-5')}>
+              <Link href={urlAutor}>
+              <a>
+              <Image width={300} height={300} src={pozaAutor.url} />
+              </a>
+              </Link>
+
+            </div>
+            <div className={tw('text-center m-5')}>
+              <h5>
+                Articol revizuit de{' '}
+                <Link href={urlAutor}><a>
+                <span>
+                  {author?.node?.lastName} {author?.node?.firstName}
+                </span>
+                  </a></Link>
+                {" "}<span className={tw('text-bold')}>{functie}</span>
+              </h5>
+            </div>
+          </aside>
         </div>
-        </>
-        
-      
+      </>
     </BlogPostContext.Provider>
   );
 };
