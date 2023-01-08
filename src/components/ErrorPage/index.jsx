afbb866a7f5f51df6bf579e40c474cb1e7ef4c95
MarcGames1 <George.marcu20@gmail.com>
2023-01-08 16:45:54 +0200

    x

--- src/components/ErrorPage/index.jsx
+++ src/components/ErrorPage/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Image from 'next/image';
+import { tw } from 'twind';
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>An error has occurred</h1>
-      <p>Sorry, it looks like something went wrong. Please try again later.</p>
+    <div className="flex container items-center">
+      <span className={tw('hidden lg:flex text-red-500 font-bold text-9xl')}>404</span>
+
+      <div className={tw('p-5 h-2/6 flex flex-col m-2 m-auto h-2/6')}>
+        <h1 className={tw('text-center m-3 font-semibol text-3xl')}>Ooops ... avem o mica problema tehnica</h1>
+        <Image objectFit="contain" layout="intrinsic" width={700} height={400} src="/loading.gif" />
+      </div>
     </div>
   );
 };
--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -1,8 +1,7 @@
 import Page from '@/components/page';
 import Head from 'next/head';
 import React, { Suspense } from 'react';
-import Image from 'next/image';
-import { tw } from 'twind';
+import ErrorPage from '@/components/ErrorPage'
 export default function Home() {
   return (
     <Suspense fallback={`Loading...`}>
@@ -10,16 +9,7 @@ export default function Home() {
         <meta name="description" content="" />
         <meta name="robots" content="noindex, follow" />
       </Head>
-      <Page>
-        <div className="flex container items-center">
-          <span className={tw('hidden lg:flex text-red-500 font-bold text-9xl')}>404</span>
-
-          <div className={tw('p-5 h-2/6 flex flex-col m-2 m-auto h-2/6')}>
-            <h1 className={tw('text-center m-3 font-semibol text-3xl')}>Ooops ... avem o mica problema tehnica</h1>
-            <Image objectFit="contain" layout="intrinsic" width={700} height={400} src="/loading.gif" />
-          </div>
-        </div>
-      </Page>
+      <Page><ErrorPage /></Page>
     </Suspense>
   );
 }
